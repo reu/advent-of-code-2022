db0daff5f04b7e5754e4d56c00bdbc1893694ab5
Rodrigo Navarro <rnavarro1@gmail.com>
2022-12-06 09:01:44 -0300

    Refactor day 5

--- src/day-05.rs
+++ src/day-05.rs
@@ -1,7 +1,8 @@
 use advent::read_input;
 use itertools::Itertools;
+use once_cell::sync::Lazy;
 use regex::Regex;
-use std::{collections::HashMap, str::FromStr};
+use std::{collections::HashMap, iter::FromIterator, str::FromStr};
 
 fn main() -> std::io::Result<()> {
     println!("Part1: {}", part1(read_input("inputs/day-05.txt")?));
@@ -10,123 +11,139 @@ fn main() -> std::io::Result<()> {
 }
 
 fn part1(mut input: impl Iterator<Item = String>) -> String {
-    let stack_input = input
-        .by_ref()
-        .take_while(|line| !line.is_empty())
-        .collect::<Vec<String>>();
-
-    let mut stacks = parse_stacks(stack_input);
+    let mut stacks = Stacks::from_iter(input.by_ref().take_while(|line| !line.is_empty()));
 
     for command in input.filter_map(|command| Command::from_str(&command).ok()) {
         for _ in 1..=command.quantity {
-            if let Some(item) = stacks
-                .get_mut(&command.source)
-                .and_then(|stack| stack.pop())
-            {
-                if let Some(to) = stacks.get_mut(&command.destination) {
-                    to.push(item);
-                };
+            if let Some(item) = stacks.pop(command.src) {
+                stacks.push(command.dst, item);
             }
         }
     }
 
     stacks
         .into_iter()
-        .sorted_by_key(|(id, _)| *id)
-        .filter_map(|(_, items)| items.last().map(|item| item.to_owned()))
+        .filter_map(|(_, items)| items.last().cloned())
         .collect()
 }
 
 fn part2(mut input: impl Iterator<Item = String>) -> String {
-    let stack_input = input
-        .by_ref()
-        .take_while(|line| !line.is_empty())
-        .collect::<Vec<String>>();
-
-    let mut stacks = parse_stacks(stack_input);
+    let mut stacks = Stacks::from_iter(input.by_ref().take_while(|line| !line.is_empty()));
 
     for command in input.filter_map(|command| Command::from_str(&command).ok()) {
-        if let Some(mut removed) = stacks.get_mut(&command.source).map(|stack| {
-            stack
-                .drain(stack.len() - command.quantity..)
-                .collect::<Vec<_>>()
-        }) {
-            if let Some(stack) = stacks.get_mut(&command.destination) {
-                stack.append(&mut removed);
-            }
+        if let Some(removed) = stacks.pop_stack(command.src, command.quantity) {
+            stacks.push_stack(command.dst, removed);
         }
     }
 
     stacks
         .into_iter()
-        .sorted_by_key(|(id, _)| *id)
-        .filter_map(|(_, items)| items.last().map(|item| item.to_owned()))
+        .filter_map(|(_, items)| items.last().cloned())
         .collect()
 }
 
 struct Command {
-    source: usize,
-    destination: usize,
+    src: usize,
+    dst: usize,
     quantity: usize,
 }
+
+static COMMAND_REGEX: Lazy<Regex> =
+    Lazy::new(|| Regex::new(r"move (\d+) from (\d+) to (\d+)").unwrap());
 
 impl FromStr for Command {
     type Err = &'static str;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let re = Regex::new(r"move (\d+) from (\d+) to (\d+)").unwrap();
-
-        let caps = re.captures(s).ok_or("Invalid command")?;
-
-        let quantity = caps
-            .get(1)
-            .and_then(|q| q.as_str().parse::<usize>().ok())
-            .ok_or("Invalid command quantity")?;
-
-        let source = caps
-            .get(2)
-            .and_then(|q| q.as_str().parse::<usize>().ok())
-            .ok_or("Invalid command source")?;
-
-        let destination = caps
-            .get(3)
-            .and_then(|q| q.as_str().parse::<usize>().ok())
-            .ok_or("Invalid command destination")?;
-
+        let caps = COMMAND_REGEX.captures(s).ok_or("Invalid command")?;
+        let amt = caps[1].parse().map_err(|_| "Invalid quantity")?;
+        let src = caps[2].parse().map_err(|_| "Invalid src")?;
+        let dst = caps[3].parse().map_err(|_| "Invalid dst")?;
         Ok(Command {
-            source,
-            destination,
-            quantity,
+            src,
+            dst,
+            quantity: amt,
         })
     }
 }
 
-fn parse_stacks(input: Vec<String>) -> HashMap<usize, Vec<String>> {
-    let re = Regex::new(r"\[([A-Z])\]").unwrap();
+#[derive(Debug, Default)]
+struct Stacks(HashMap<usize, Vec<String>>);
 
-    let (ids, items) = input.split_last().unwrap();
+impl Stacks {
+    fn push(&mut self, stack: usize, item: String) {
+        if let Some(stack) = self.0.get_mut(&stack) {
+            stack.push(item);
+        }
+    }
 
-    let ids = Regex::new(r"(\d+)")
-        .unwrap()
-        .captures_iter(ids)
-        .filter_map(|cap| cap.get(1))
-        .map(|item| (item.start(), item.as_str().parse::<usize>().unwrap()))
-        .collect::<HashMap<_, _>>();
+    fn push_stack(&mut self, stack: usize, mut items: Vec<String>) {
+        if let Some(stack) = self.0.get_mut(&stack) {
+            stack.append(&mut items);
+        }
+    }
 
-    #[allow(clippy::needless_collect)]
-    let items = items
-        .iter()
-        .flat_map(|line| re.captures_iter(line).filter_map(|cap| cap.get(1)))
-        .map(|item| (item.start(), item.as_str()))
-        .collect::<Vec<_>>();
+    fn pop(&mut self, stack: usize) -> Option<String> {
+        self.0.get_mut(&stack).and_then(|stack| stack.pop())
+    }
 
-    items
-        .into_iter()
-        .rev()
-        .fold(HashMap::new(), |mut stacks, (col, item)| {
-            stacks.entry(ids[&col]).or_default().push(item.to_string());
-            stacks
-        })
+    fn pop_stack(&mut self, stack: usize, size: usize) -> Option<Vec<String>> {
+        self.0
+            .get_mut(&stack)
+            .map(|stack| stack.drain(stack.len() - size..).collect())
+    }
+}
+
+impl IntoIterator for Stacks {
+    type Item = (usize, Vec<String>);
+    type IntoIter = Box<dyn Iterator<Item = Self::Item>>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        Box::new(self.0.into_iter().sorted_by_key(|(id, _)| *id))
+    }
+}
+
+static STACKS_REGEX: Lazy<Regex> = Lazy::new(|| Regex::new(r"\[([A-Z])\]").unwrap());
+static STACKS_IDS_REGEX: Lazy<Regex> = Lazy::new(|| Regex::new(r"(\d+)").unwrap());
+
+impl FromIterator<String> for Stacks {
+    fn from_iter<T: IntoIterator<Item = String>>(iter: T) -> Self {
+        let input = iter.into_iter();
+        let (items, mut ids) = input.tee();
+
+        let ids = ids
+            .find(|line| !STACKS_REGEX.is_match(line))
+            .map(|line| {
+                STACKS_IDS_REGEX
+                    .captures_iter(&line)
+                    .filter_map(|cap| cap.get(1))
+                    .map(|item| (item.start(), item.as_str().parse::<usize>().unwrap()))
+                    .collect::<HashMap<_, _>>()
+            })
+            .unwrap_or_default();
+
+        let items = items
+            .take_while(|line| STACKS_REGEX.is_match(line))
+            .flat_map(|line| {
+                STACKS_REGEX
+                    .captures_iter(&line)
+                    .filter_map(|cap| cap.get(1))
+                    .map(|item| (item.start(), item.as_str().to_owned()))
+                    .collect::<Vec<_>>()
+            })
+            .collect::<Vec<_>>()
+            .into_iter()
+            .rev()
+            .fold(
+                HashMap::<usize, Vec<String>>::new(),
+                |mut stacks, (col, item)| {
+                    stacks.entry(ids[&col]).or_default().push(item);
+                    stacks
+                },
+            );
+
+        Stacks(items)
+    }
 }
 
 #[cfg(test)]
